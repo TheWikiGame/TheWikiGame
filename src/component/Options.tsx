b3ef7a2b9a6a2d61505a7f877e20e76ea02d2d60
ZachElkins <zach.elkins@gmil.com>
2024-08-26 15:53:08 -0700

    Options fuzzy search

--- src/component/Options.tsx
+++ src/component/Options.tsx
@@ -1,16 +1,35 @@
+import { useCallback, useMemo, useState } from "react"
 import { Page } from "../model/page"
+import Fuse from "fuse.js"
 
 type OptionsProps = {
     pages: Page[]
 } & React.ComponentProps<'div'>
-
 export const Options = ({className, pages, ...props}: OptionsProps) => {
+    const [searchText, setSearchText] = useState('')
+
+
+    const fuse = useMemo(() => new Fuse(pages, {
+        keys: ['title', 'page']
+      }), [pages]);
+      
+      const filteredPages = useMemo(() => {
+        return searchText ? fuse.search(searchText).map(result => result.item) : pages;
+      }, [fuse, searchText, pages]);
+
+      const handleSearchChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchText(event.target.value);
+      }, []);
+
     return (
         <div className={`${className}`} {...props}>
             <h2>Choices</h2>
-            {pages.map(page => (
-                <a href={page.href}>{page.title}</a>
-            ))}
+            <input type="text" value={searchText} onChange={handleSearchChange}/>
+            <div className={'flex flex-col'}>
+                {filteredPages.map(page => (
+                    <a key={page.page} href={page.href}>{page.title}</a>
+                ))}
+            </div>
         </div>
     )
 }
