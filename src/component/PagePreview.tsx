67a8931bc0b3a89f8e6203a58708e7ee8efeaa75
ZachElkins <zach.elkins@gmail.com>
2024-08-27 16:32:06 -0700

    PagePreview iFrame

---
+++ src/component/PagePreview.tsx
@@ -0,0 +1,25 @@
+import { Page } from "../model/Page";
+
+type PagePreviewProps = {
+  page: Page;
+} & React.ComponentProps<"div">;
+
+export const PagePreview = ({
+  className,
+  page,
+  ...props
+}: PagePreviewProps) => {
+  return (
+    <div
+      className={`w-full border border-[--var(--text-secondary)] ${className}`}
+      {...props}
+    >
+      <iframe
+        src={page.href}
+        className="w-full h-full"
+        title={page.title}
+        sandbox="allow-scripts"
+      ></iframe>
+    </div>
+  );
+};
