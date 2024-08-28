5c4a767c64dae1ac2d79d3a201720e0f5c21272a
ZachElkins <zach.elkins@gmail.com>
2024-08-28 11:00:33 -0700

    Style changes

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -3,11 +3,16 @@ type ButtonProps = {
 } & React.ComponentProps<"button">;
 
 export const Button = ({ className, variant, ...props }: ButtonProps) => {
-  const primaryStyle = "bg-[var(--text-secondary)] text-[var(--bg-primary)]";
-  const secondaryStyle = "bg-[var(--bg-secondary)]";
+  const primaryStyle =
+    "bg-[var(--bg-accent)] text-[var(--bg-primary)] hover:bg-[var(--bg-accent-hover)] active:bg-[var(--bg-accent-active)]";
+  const secondaryStyle =
+    "bg-[var(--bg-secondary)] hover:bg-[var(--bg-secondary-hover)] active:bg-[var(--bg-secondary-active)]";
 
   const style = variant === "primary" ? primaryStyle : secondaryStyle;
   return (
-    <button className={`px-2 py-1 rounded ${style} ${className}`} {...props} />
+    <button
+      className={`px-2 py-1 rounded transition-colors duration-75 ${style} ${className}`}
+      {...props}
+    />
   );
 };
--- src/component/InlinePage.tsx
+++ src/component/InlinePage.tsx
@@ -1,4 +1,3 @@
-import { ExternalLink } from "lucide-react";
 import { Page } from "../model/Page";
 
 type InlinePageProps = {
@@ -11,12 +10,9 @@ export const InlinePage = ({ className, page, ...props }: InlinePageProps) => {
       {...props}
       href={page.href}
       target="_blank"
-      className={`${className} inline-flex w-fit text-[var(--text-secondary)] hover:text-[var(--text-secondary-hover)] underline`}
+      className={`${className} inline-flex w-fit text-[var(--text-secondary)] hover:text-[var(--text-secondary-hover)] hover:underline`}
     >
-      <span>{page.title}</span>
-      <span className="pl-1">
-        <ExternalLink size={10} className="inline align-middle mb-1" />
-      </span>
+      {page.title}
     </a>
   );
 };
