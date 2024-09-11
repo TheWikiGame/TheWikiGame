0e6284f5835fc27802f7707181da1f3cb47fbd3f
ZachElkins <zach.elkins@gmail.com>
2024-09-10 23:15:38 -0700

    Added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import "./App.css";
 import { Game } from "./page/Game";
 import { PageLayout } from "./page/PageLayout";
 import { ThemeProvider } from "./theme-provider";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <PageLayout header={"The Wiki Game"} content={<Game />} />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<PageLayout header={"The Wiki Game"} content={<Game />} />}
+          />
+          <Route
+            path="/:game_id"
+            element={<PageLayout header={"The Wiki Game"} content={<Game />} />}
+          />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
